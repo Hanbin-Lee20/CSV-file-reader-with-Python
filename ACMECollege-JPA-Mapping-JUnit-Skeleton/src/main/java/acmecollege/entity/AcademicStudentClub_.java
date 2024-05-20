9a6625e03acb8851f8c109976bbb0e95f6634737
HanbinLee <92098748+Hanbin-Lee20@users.noreply.github.com>
2024-05-20 19:29:06 -0400

    Delete ACMECollege-JPA-Mapping-JUnit-Skeleton directory
    Unnecessary directory removed

--- ACMECollege-JPA-Mapping-JUnit-Skeleton/src/main/java/acmecollege/entity/AcademicStudentClub_.java
+++
@@ -1,9 +0,0 @@
-package acmecollege.entity;
-
-import javax.annotation.Generated;
-import javax.persistence.metamodel.StaticMetamodel;
-
-@Generated(value="Dali", date="2024-03-18T11:20:29.139-0400")
-@StaticMetamodel(AcademicStudentClub.class)
-public class AcademicStudentClub_ extends StudentClub_ {
-}
